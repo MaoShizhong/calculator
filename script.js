a4c8cb3c50feff8e5132b4e6cea37d153c825de0
Alga Mau <122839503+MaoShizhong@users.noreply.github.com>
2023-03-14 15:54:52 +0000

    Enable layout switching

---
+++ script.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll(".basic");
+const layout = document.querySelector("#slider");
+
+layout.addEventListener("click", function() {
+    const extraButtons = document.querySelectorAll(".extra");
+    extraButtons.forEach(extraButton => extraButton.classList.toggle("hidden"));
+
+    buttons.forEach(button => {
+        button.classList.toggle("basic");
+        button.classList.toggle("extended");
+    });
+
+    toggleSlider();
+});
+
+function toggleSlider() {
+    layout.classList.toggle("on");
+    if (layout.classList.contains("on")) {
+        layout.firstChild.src = "./images/ext-on.png";
+    }
+    else {
+        layout.firstChild.src = "./images/ext-off.png";
+    }
+}
